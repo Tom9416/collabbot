eecf84aeb58ea907134fcd0257939e781e4cc0e1
Tom Parkin <thomas.parkin@bglgroup.co.uk>
2018-08-09 09:54:28 +0100

    Updated naming

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,21 +6,21 @@ const config = require('../config')
 
 const msgDefaults = {
   response_type: 'in_channel',
-  username: 'Starbot',
+  username: 'collabhelp',
   icon_emoji: config('ICON_EMOJI')
 }
 
 let attachments = [
   {
-    title: 'Starbot will help you find the hippest repos on GitHub',
+    title: 'Collabhelp will help you find all the collaboration tools you need',
     color: '#2FA44F',
-    text: '`/starbot repos` returns hip repos \n`/starbot javascript` returns hip JavaScript repos',
+    text: '`/collabhelp help` returns all the commands you can do \n`/collabhelp how to use lifesize` will show you how to use lifesize',
     mrkdwn_in: ['text']
   },
   {
-    title: 'Configuring Starbot',
+    title: 'Configuring Collabhelp',
     color: '#E3E4E6',
-    text: '`/starbot help` ... you\'re lookin at it! \n',
+    text: '`/collabhelp help` ... you\'re lookin at it! \n',
     mrkdwn_in: ['text']
   }
 ]
